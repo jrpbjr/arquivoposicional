4c958c50b49b8c2eefde2c2c76326b2760dd3dfd
jrpbjr <jrpbjr@gmail.com>
2025-05-13 15:57:05 -0300

    first commit

---
+++ src/test/java/br/com/jrpbjr/arquivoposicional/ArquivoposicionalApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jrpbjr.arquivoposicional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArquivoposicionalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
